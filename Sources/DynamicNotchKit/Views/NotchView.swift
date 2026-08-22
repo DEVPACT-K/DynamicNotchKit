3c44e32fdf3bf65ea8a15e375b6fb5af4b0c61ab
DevPact <karlochieng17@gmail.com>
2026-08-22 23:24:09 +0300

    monterey: correct onGeometryChange backport

--- Sources/DynamicNotchKit/Views/NotchView.swift
+++ Sources/DynamicNotchKit/Views/NotchView.swift
@@ -134,7 +134,7 @@ struct NotchView<Expanded, CompactLeading, CompactTrailing, CompactBottom>: View
                         .safeAreaInset(edge: .leading, spacing: 0) { Color.clear.frame(width: 8) }
                         .safeAreaInset(edge: .top, spacing: 0) { Color.clear.frame(height: 4) }
                         .safeAreaInset(edge: .bottom, spacing: 0) { Color.clear.frame(height: 8) }
-                        .background(GeometryReader { g in Color.clear.onChange(of: g.size.compactLeadingWidth) { width = g.size.compactLeadingWidth } })
+                        .background(GeometryReader { g in Color.clear.onChange(of: g.size.width) { compactLeadingWidth = g.size.width } })
                         .frame(width: compactSideWidth, alignment: .trailing)
                         .transition(.blur(intensity: 10).combined(with: .scale(x: 0, anchor: .trailing)).combined(with: .opacity))
                 }
@@ -148,7 +148,7 @@ struct NotchView<Expanded, CompactLeading, CompactTrailing, CompactBottom>: View
                         .safeAreaInset(edge: .trailing, spacing: 0) { Color.clear.frame(width: 8) }
                         .safeAreaInset(edge: .top, spacing: 0) { Color.clear.frame(height: 4) }
                         .safeAreaInset(edge: .bottom, spacing: 0) { Color.clear.frame(height: 8) }
-                        .background(GeometryReader { g in Color.clear.onChange(of: g.size.compactTrailingWidth) { width = g.size.compactTrailingWidth } })
+                        .background(GeometryReader { g in Color.clear.onChange(of: g.size.width) { compactTrailingWidth = g.size.width } })
                         .frame(width: compactSideWidth, alignment: .leading)
                         .transition(.blur(intensity: 10).combined(with: .scale(x: 0, anchor: .leading)).combined(with: .opacity))
                 }
@@ -158,7 +158,7 @@ struct NotchView<Expanded, CompactLeading, CompactTrailing, CompactBottom>: View
             if dynamicNotch.state == .compact, !dynamicNotch.disableCompactBottom {
                 dynamicNotch.compactBottomContent
                     .environment(\.notchSection, .compactBottom)
-                    .background(GeometryReader { g in Color.clear.onChange(of: g.size.compactBottomHeight) { height = g.size.compactBottomHeight } })
+                    .background(GeometryReader { g in Color.clear.onChange(of: g.size.height) { compactBottomHeight = g.size.height } })
                     .transition(.blur(intensity: 10).combined(with: .scale(y: 0, anchor: .top)).combined(with: .opacity))
             }
         }
